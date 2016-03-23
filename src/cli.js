2437f8ab6b032aa0455ec98bbed6fb47f1bdc6bb
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-03-23 12:31:33 +0100

    Use dthrong to scale processor wise

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,4 @@
 #! /usr/bin/env node
 'use strict';
 
-require('dnewrelic');
-
-let app = require('dexpress');
-app.use(require('dheaders'));
-const dhttpAuth = require('dhttp-auth');
-if (dhttpAuth) {
-  app.use(dhttpAuth);
-}
-app.use(require('compression')());
-app.use(app.express.static(process.cwd() + '/www'));
+require('dthrong')(__dirname + '/dwebserver.js');
---
+++ src/dwebserver.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dnewrelic');
+
+let app = require('dexpress');
+app.use(require('dheaders'));
+const dhttpAuth = require('dhttp-auth');
+if (dhttpAuth) {
+  app.use(dhttpAuth);
+}
+app.use(require('compression')());
+app.use(app.express.static(process.cwd() + '/www'));
